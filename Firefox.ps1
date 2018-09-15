a7da844b6c724ce2b7576e94f3e923799ca78876
Bronson Magnan <33496959+BronsonMagnan@users.noreply.github.com>
2018-09-14 21:10:10 -0400

    Updating code to use Mozilla JSON feed - thanks Aaron Parker.

--- Firefox.ps1
+++ Firefox.ps1
@@ -8,9 +8,13 @@ param(
 
 )
 
-$FFReleaseNoticeURL = "https://www.mozilla.org/en-US/firefox/releases/"
-$FFLatestVersion = ((wget -uri $FFReleaseNoticeURL | % content).split() | Select-String -Pattern 'data-latest-firefox="*"').tostring().split('"')[1]
-$VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($FFLatestVersion)/$($architecture)/$($culture)/Firefox%20Setup%20$($FFLatestVersion).exe"
+# JSON that provide details on Firefox versions
+$uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
+
+# Read the JSON and convert to a PowerShell object
+$firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
+
+$VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($firefoxVersions.LATEST_FIREFOX_VERSION)/$($architecture)/$($culture)/Firefox%20Setup%20$($FFLatestVersion).exe"
 Write-Output $VersionURL
 }
 
