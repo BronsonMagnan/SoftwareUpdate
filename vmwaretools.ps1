73c44a6cb923b11601da8618e1c4b5a29cf33a4d
Bronson Magnan <33496959+BronsonMagnan@users.noreply.github.com>
2018-09-16 15:58:31 -0400

    Added functions
    Added functions to get the url, and compare a installer with the current web version

--- vmwaretools.ps1
+++ vmwaretools.ps1
@@ -16,4 +16,38 @@
     Write-Output $version
 }
 
-Get-VMWareToolsVersion
+function Get-VMWareToolsURL {
+    [cmdletbinding()]
+    [outputtype([string])]
+    $vmwaretools = "https://packages.vmware.com/tools/esx/latest/windows/x64/index.html"
+    $pattern = "[0-9]+\.[0-9]+\.[0-9]+\-[0-9]+\-x86_64"
+    #get the raw page content
+    $pagecontent=(wget -Uri $vmwaretools).content
+    #change one big string into many strings, then find only the line with the version number
+    $interestingLine = ($pagecontent.split("`n") | Select-string -Pattern $pattern).tostring().trim()
+    #remove the whitespace and split on the assignment operator, then split on the double quote and select the correct item
+    $filename = (($interestingLine.replace(" ","").split("=") | Select-string -Pattern $pattern).tostring().trim().split("`""))[1]
+    $url = "https://packages.vmware.com/tools/esx/latest/windows/x64/$($filename)"
+    Write-Output $url
+}
+
+function Check-ForLatestVMWareTools {
+[cmdletbinding()]
+[outputtype([boolean])]
+param (
+    [parameter(mandatory=$true)]
+    [validateNotNullOrEmpty()]
+    [string]$path
+)
+    $fileversion = [version](Get-ChildItem -Path $path | Select-Object -ExpandProperty versioninfo).fileversion
+    $currentversion = Get-VMWareToolsVersion
+    if ($currentversion -gt $fileversion) {
+        return $false
+    } else {
+        return $true
+    }
+}
+
+
+#Example: Check-ForLatestVMWareTools -path .\tools.exe
+
