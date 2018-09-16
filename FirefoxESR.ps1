b3d1c25b98030460aa754037cd1de9a34465632f
Bronson Magnan <33496959+BronsonMagnan@users.noreply.github.com>
2018-09-16 15:18:31 -0400

    Updated for ESR url difference

--- FirefoxESR.ps1
+++ FirefoxESR.ps1
@@ -3,7 +3,7 @@
 [outputtype([String])]
 param(
     [ValidateSet("bn-BD","bn-IN","en-CA","en-GB","en-ZA","es-AR","es-CL","es-ES","es-MX")][string]$culture = "en-US",
-    [ValidateSet("win32","win64")][string]$architecture="win32"
+    [ValidateSet("win32","win64")][string]$architecture="win64"
 
 )
 
@@ -13,7 +13,7 @@ $uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
 # Read the JSON and convert to a PowerShell object
 $firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
 
-$VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($firefoxVersions.FIREFOX_ESR)/$($architecture)/$($culture)/Firefox%20Setup%20$($FFLatestVersion).exe"
+$VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($firefoxVersions.FIREFOX_ESR)/$($architecture)/$($culture)/Firefox%20Setup%20$($firefoxVersions.FIREFOX_ESR).exe"
 Write-Output $VersionURL
 }
 
@@ -22,7 +22,7 @@ function get-LatestFirefoxESRVersion {
 [outputtype([String])]
 param(
     [ValidateSet("bn-BD","bn-IN","en-CA","en-GB","en-ZA","es-AR","es-CL","es-ES","es-MX")][string]$culture = "en-US",
-    [ValidateSet("win32","win64")][string]$architecture="win32"
+    [ValidateSet("win32","win64")][string]$architecture="win64"
 
 )
 
@@ -38,3 +38,4 @@ Write-Output $Version
 
 get-LatestFirefoxESRURL
 get-LatestFirefoxESRVersion
+
