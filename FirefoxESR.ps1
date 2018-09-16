ada9ca2f64fd97a9dd10ea048653cd99986046e4
Bronson Magnan <33496959+BronsonMagnan@users.noreply.github.com>
2018-09-16 15:01:16 -0400

    FireFox ESR Version and URL

---
+++ FirefoxESR.ps1
@@ -0,0 +1,40 @@
+﻿function get-LatestFirefoxESRURL {
+[cmdletbinding()]
+[outputtype([String])]
+param(
+    [ValidateSet("bn-BD","bn-IN","en-CA","en-GB","en-ZA","es-AR","es-CL","es-ES","es-MX")][string]$culture = "en-US",
+    [ValidateSet("win32","win64")][string]$architecture="win32"
+
+)
+
+# JSON that provide details on Firefox versions
+$uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
+
+# Read the JSON and convert to a PowerShell object
+$firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
+
+$VersionURL = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$($firefoxVersions.FIREFOX_ESR)/$($architecture)/$($culture)/Firefox%20Setup%20$($FFLatestVersion).exe"
+Write-Output $VersionURL
+}
+
+function get-LatestFirefoxESRVersion {
+[cmdletbinding()]
+[outputtype([String])]
+param(
+    [ValidateSet("bn-BD","bn-IN","en-CA","en-GB","en-ZA","es-AR","es-CL","es-ES","es-MX")][string]$culture = "en-US",
+    [ValidateSet("win32","win64")][string]$architecture="win32"
+
+)
+
+# JSON that provide details on Firefox versions
+$uriSource = "https://product-details.mozilla.org/1.0/firefox_versions.json"
+
+# Read the JSON and convert to a PowerShell object
+$firefoxVersions = (Invoke-WebRequest -uri $uriSource).Content | ConvertFrom-Json
+
+$Version = [Version]$firefoxVersions.FIREFOX_ESR.replace("esr","")
+Write-Output $Version
+}
+
+get-LatestFirefoxESRURL
+get-LatestFirefoxESRVersion
