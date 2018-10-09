c72c1c0b6bcc95ed453bbde8d78b84ebd920d0fe
Bronson Magnan <33496959+BronsonMagnan@users.noreply.github.com>
2018-10-09 09:31:58 -0400

    Notepad Plus Plus

---
+++ NotePadPlusPlus.ps1
@@ -0,0 +1,26 @@
+﻿function get-NPPCurrentVersion {
+    [cmdletbinding()]
+    [outputtype([version])]
+    $url = "https://notepad-plus-plus.org/"
+    $content = wget -Uri $url
+    #pipe this match to out-null so it does not corrupt the pipeline
+    ($content.allelements | ? id -eq "download").innerText -match "\d+\.\d+\.\d+" | out-null
+    $ver = [version]::new($Matches[0])
+    Write-Output $ver
+}
+
+function get-NPPCurrentDownloadURL {
+    [cmdletbinding()]
+    [outputtype([string])]
+    param (
+        [validateSet("x86","x64")][string]$Architecture = "x64"
+    )
+    $version = get-NPPCurrentVersion
+    if ("x86" -eq $Architecture) { $archcode = "" } else { $archcode = ".x64" }
+    $url = "https://notepad-plus-plus.org/repository/$($version.major).x/$version/npp.$($version).Installer$($archcode).exe"
+    Write-Output $url
+}
+
+#Example get-NPPCurrentDownloadURL -Architecture x64
+
+#Example wget -uri (get-NPPCurrentDownloadURL) -OutFile .\npp.exe
